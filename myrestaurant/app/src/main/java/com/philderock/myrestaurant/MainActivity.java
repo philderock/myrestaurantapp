30ef652dddb602ed42c58d5f6cbcd0c03a6dc1da
Philip Chukwuma <cchukwuma.p@gmail.com>
2022-10-23 17:19:26 +0100

    Added new activity

--- myrestaurant/app/src/main/java/com/philderock/myrestaurant/MainActivity.java
+++ myrestaurant/app/src/main/java/com/philderock/myrestaurant/MainActivity.java
@@ -1,14 +1,32 @@
 package com.philderock.myrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    CardView startersCard;
+    CardView mainsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starter);
+        mainsCard = findViewById(R.id.card_view_mains);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+
+                startActivity(startersActivityIntent);
+            }
+        });
+
     }
 }
